31a5d578424f7e2a74f726e2e2a64a1a1a06daf4
kinley05 <tsheringk127@gmail.com>
2025-08-12 15:03:17 +0600

    csf

---
+++ basics/strings.py
@@ -0,0 +1,11 @@
+name = "John Doe"
+print(name)
+
+greeting = "Hello, " + name + "!"
+print(greeting)
+
+greeting_f = f"hello, {name}!"
+print(greeting_f)
+
+name_length = len(name)
+print(name_length)
---
+++ function_and_scope/basic_functions.py
@@ -0,0 +1,9 @@
+def greet():
+    print("Hello, World")
+
+greet()
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
